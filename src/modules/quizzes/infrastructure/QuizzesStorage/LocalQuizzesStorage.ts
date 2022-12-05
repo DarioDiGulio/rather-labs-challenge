37adf2389c5d18b159c4cd37b66b591157ab8baf
Dario Di Gulio <dariodigulio@gmail.com>
2022-12-05 09:49:07 -0300

    Fix Build

--- src/modules/quizzes/infrastructure/QuizzesStorage/LocalQuizzesStorage.ts
+++ src/modules/quizzes/infrastructure/QuizzesStorage/LocalQuizzesStorage.ts
@@ -1,14 +1,8 @@
 import { QuizzesStorage } from '@/modules/quizzes/infrastructure/QuizzesStorage/QuizzesStorage'
 import { Quizz } from '@/modules/quizzes/models/Quizz'
-import fs from 'fs'
 
 export class LocalQuizzesStorage implements QuizzesStorage {
     getIds(): string[] {
-        fs.readdir(`${ global.appRoot }/resources/quizzes/`, (err, files) => {
-            files.forEach(file => {
-                console.log(file)
-            })
-        })
         return []
     }
 
